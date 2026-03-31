ce579f1e6391a091e79ca21db785b34b7e7dd399
Sanjib Chakraborty <Sanjib.Chakraborty@amway.com>
2026-03-31 16:49:15 +0530

    Added pipeline

---
+++ PipelineGeneric/PipelineGeneric/PipelineGenericApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineGenericApp.swift
+//  PipelineGeneric
+//
+//  Created by Sanjib Chakraborty on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PipelineGenericApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PipelineGeneric/PipelineGenericTests/PipelineGenericTests.swift
@@ -0,0 +1,17 @@
+//
+//  PipelineGenericTests.swift
+//  PipelineGenericTests
+//
+//  Created by Sanjib Chakraborty on 31/03/26.
+//
+
+import Testing
+@testable import PipelineGeneric
+
+struct PipelineGenericTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
